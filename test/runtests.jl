c327884224c8cf0e309a5d269ed39dadb19bd8d2
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2025-02-05 11:11:30 -0800

    Do complement of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,54 +16,54 @@ end
 
 # Performance and code quality tests
 @safetestset "Quality Assurance tests" begin
-    include("quality_assurance.jl")
+    # include("quality_assurance.jl")
 end
 
 # Unit tests
 @safetestset "Utils tests" begin
-    include("utils.jl")
+    # include("utils.jl")
 end
 
 @safetestset "OutputPathGenerator tests" begin
-    include("output_path_generator.jl")
+    # include("output_path_generator.jl")
 end
 
 @safetestset "TimeManager tests" begin
-    include("timemanager.jl")
+    # include("timemanager.jl")
 end
 
 @safetestset "DataStructures tests" begin
-    include("data_structures.jl")
+    # include("data_structures.jl")
 end
 
 @safetestset "FileReaders tests" begin
-    include("file_readers.jl")
+    # include("file_readers.jl")
 end
 
 @safetestset "Regridders tests" begin
-    include("regridders.jl")
+    # include("regridders.jl")
 end
 
 @safetestset "DataHandling tests" begin
-    include("data_handling.jl")
+    # include("data_handling.jl")
 end
 
 @safetestset "SpaceVaryingInputs tests" begin
-    include("space_varying_inputs.jl")
+    # include("space_varying_inputs.jl")
 end
 
 @safetestset "TimeVaryingInputs tests" begin
-    include("time_varying_inputs.jl")
+    # include("time_varying_inputs.jl")
 end
 
 @safetestset "TimeVaryingInputs23D tests" begin
-    # include("time_varying_inputs23.jl")
+    include("time_varying_inputs23.jl")
 end
 
 @safetestset "TimeVaryingInputs tests LinearPeriodFilling" begin
-    include("time_varying_inputs_linearperiodfilling.jl")
+    # include("time_varying_inputs_linearperiodfilling.jl")
 end
 
 @safetestset "ClimaArtifacts tests" begin
-    include("clima_artifacts.jl")
+    # include("clima_artifacts.jl")
 end
