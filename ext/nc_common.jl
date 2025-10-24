873d400a2ee6937a4e56149c74e0636f4fe8c977
nefrathenrici <nat.henrici@gmail.com>
2025-10-23 22:03:37 -0600

    add valid time

--- ext/nc_common.jl
+++ ext/nc_common.jl
@@ -9,9 +9,11 @@ Return all the dates in the given NCDataset. The dates are read from the "time"
 or "date" datasets. If none is available, return an empty vector.
 """
 function read_available_dates(ds::NetCDFDataset)
-    if "time" in keys(ds.dim)
+    is_time = x -> x == "time" || x == "t" || x == "valid_time"
+    time_dims = filter(is_time, NCDatasets.dimnames(ds))
+    if !isempty(time_dims)
         return Dates.DateTime.(
-            reinterpret.(Ref(NCDatasets.DateTimeStandard), ds["time"][:]),
+            reinterpret.(Ref(NCDatasets.DateTimeStandard), ds[first(time_dims)][:]),
         )
     elseif "date" in keys(ds.dim)
         return Dates.DateTime.(string.(ds["date"][:]), Ref("yyyymmdd"))
