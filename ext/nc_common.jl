befedb88ae9e69a1b72ebcca605797ee906fd93c
ph-kev <98072684+ph-kev@users.noreply.github.com>
2026-07-16 10:53:26 -0700

    Add TIME_NAMES constant

--- ext/nc_common.jl
+++ ext/nc_common.jl
@@ -1,3 +1,6 @@
+# Names a temporal dimension might have in a NetCDF file
+const TIME_NAMES = ("time", "date", "t", "valid_time")
+
 # For a single and multi-file dataset
 const NetCDFDataset =
     Union{NCDatasets.NCDataset, NCDatasets.CommonDataModel.MFDataset}
@@ -11,7 +14,9 @@ available, return an empty vector.
 """
 function read_available_dates(ds::NetCDFDataset)
     # Check for time dimensions in order of preference
-    for time_dim in ("time", "t", "valid_time")
+    for time_dim in TIME_NAMES
+        # "date" holds integer yyyymmdd values and is parsed separately below
+        time_dim == "date" && continue
         if time_dim in keys(ds.dim)
             # NCDatasets.jl uses CFTime.jl, which supports a time resolution of
             # an attosecond, whereas Dates.DateTime only supports a time
